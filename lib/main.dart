a8bdf8069b91ab55e0578b421926d14933b92545
Patrick <mailtopatrick@gmx.net>
2020-06-19 17:53:59 +0200

    Use animated avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white10,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              AvatarGlow(
+                endRadius: 120.0,
+                child: Material(
+                  elevation: 10.0,
+                  shape: CircleBorder(),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black54,
+                    child: Image.asset(
+                      'images/pat.png',
+                      height: 70,
+                    ),
+                    radius: 50.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
