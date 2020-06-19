97cbf72145757526111c3013aa4659665571f043
Patrick <mailtopatrick@gmx.net>
2020-06-19 20:10:43 +0200

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import 'Helper.dart';
+
+GetIt locator = GetIt();
 
 void main() {
+  setupLocator();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+void setupLocator() {
+  locator.registerSingleton(CallsAndMessagesService());
+}
+
+class _MyAppState extends State<MyApp> {
+  final CallsAndMessagesService _service = locator<CallsAndMessagesService>();
+
+  final String number = "+49 160 91332419";
+  final String email = "patrick.fischer.privat@gmail.com";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white10,
-        body: SafeArea(
+        body: Center(
           child: Column(
             children: <Widget>[
               AvatarGlow(
@@ -29,10 +44,64 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Patrick Fischer',
+                  style: TextStyle(
+                      fontFamily: 'PressStart2P-Regular',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  'Software Engineer',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  'Bachelor Computer Science FHNW',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(leading: Icon(Icons.phone), title: Text(number)),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(leading: Icon(Icons.mail), title: Text(email)),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
